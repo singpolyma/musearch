3f5904d88ded34e7cd1a1e146085009fea409576
Stephen Paul Weber <singpolyma@singpolyma.net>
2011-07-10 00:06:51 +0000

    simplify subscribe script

--- subscribe.rb
+++ subscribe.rb
@@ -9,10 +9,4 @@ $config = YAML::load_file(File.dirname(__FILE__) + '/config.yaml')
 # Usage: ruby subscribe.rb TOPIC USER
 
 # Send the subscribe call
-subscribe(ARGV[0], ($config['approot'].split('/') + ['pshb']).join('/'), $config['secret']) { |topic|
-	# Add the user to the file so the subscribe will succeed
-	open(File.join($config['data_dir'], u(topic)), 'a') { |fh|
-		fh.puts ARGV[1]
-	}
-	true
-}
+subscribe(ARGV[0], ($config['approot'].split('/') + ['pshb']).join('/'), $config['secret'])
