9be6220e1de6506f3c5ff87b2b050254ba1d27e5
Stephen Paul Weber <singpolyma@singpolyma.net>
2011-07-10 00:04:32 +0000

    better author and content polyglot

--- lib/xml_feed_polyglot.rb
+++ lib/xml_feed_polyglot.rb
@@ -21,13 +21,14 @@ def xml_feed_polyglot(string)
 	root.add_namespace('thr', 'http://purl.org/syndication/thread/1.0')
 
 	root.elements.each('./title') {|el| meta[:title] = ''; el.write(meta[:title]); meta[:title].gsub!(/<[^>]+>/, '').strip!}
-	root.elements.each('./link') {|el| meta[:link] = el.attributes['href'] ? el.attributes['href'].to_s : el.text}
+	root.elements.each('./link') {|el| meta[:link] = el.attributes['href'] ? el.attributes['href'].to_s : el.text; break }
 	root.elements.each('./atom:link[@rel="self"]') {|el| meta[:self] = el.attributes['href'] }
 
 	root.elements.each('./image/url|./logo') {|el| meta[:logo] = el.text}
 
 	root.elements.each('./atom:author|./author|./dc:creator') {|el|
-		if el.children.length > 0
+		n = el.children.inject(0) {|n,c| c.class == REXML::Text ? n+1 : n }
+		if n > 0
 			meta[:author] = {}
 			el.elements.each('./name') {|c| meta[:author][:fn] = c.text}
 			el.elements.each('./uri') {|c| meta[:author][:url] = c.text}
@@ -61,7 +62,11 @@ def xml_feed_polyglot(string)
 		itemel.elements.each('./link') {|el| item[:bookmark] = el.text}
 		itemel.elements.each('./atom:link[@rel="alternate"][@type="text/html"]') {|el| item[:bookmark] = el.attributes['href']}
 		itemel.elements.each('./guid|./atom:id') {|el| item[:id] = el.text}
-		itemel.elements.each('./description|./content:encoded') {|el|
+		itemel.elements.each('./content:encoded') {|el|
+			# Always end up HTML-safe
+			item[:content] = HTMLEntities.decode_entities(el.text)
+		}
+		itemel.elements.each('./description') {|el|
 			# Always end up HTML-safe
 			item[:content] = HTMLEntities.decode_entities(el.text)
 		}
@@ -78,7 +83,8 @@ def xml_feed_polyglot(string)
 			end
 		}
 		itemel.elements.each('./atom:author|./author|./dc:creator') {|el|
-			if el.children.length > 0
+			n = el.children.inject(0) {|n,c| c.class == REXML::Text ? n+1 : n }
+			if n > 0
 				item[:author] = {}
 				el.elements.each('./name') {|c| item[:author][:fn] = c.text}
 				el.elements.each('./uri') {|c| item[:author][:url] = c.text}
