f5c8c518c8423cbdd00414140b8f6a76688ea88c
Stephen Paul Weber <singpolyma@singpolyma.net>
2011-07-12 02:09:59 +0000

    Add feeds and content negotiation features

--- controllers/application.rb
+++ controllers/application.rb
@@ -20,17 +20,28 @@ class ApplicationController < HamlController
 		['/stylesheets/main.css']
 	end
 
-	def render(args={})
-		return @error if @error
+	def alternates
+		recognized_types.map do |type|
+			next if type == @content_type
+			query = @req.GET.map {|k, v|
+				next if k == '_accept'
+				"#{u(k)}=#{u(v)}"
+			}.join('&')
+			{:type => type, :href => $config['approot'] + '?' + query + '&_accept='+u(type)}
+		end.compact
+	end
 
-		args[:content_type] = @req.accept_media_types.select { |type|
+	def render(args={})
+		@content_type = args[:content_type] = @req['_accept'] || 
+		                @req.accept_media_types.select {|type|
 			recognized_types.index(type)
 		}.first
 
-		#case args[:content_type]
-		#	else
-				args[:content_type] += '; charset=utf-8' if args[:content_type]
-				super(args)
-		#end
+		args[:content_type] += '; charset=utf-8' if args[:content_type]
+		r = super(args)
+
+		# Cache headers. Varnish likes Cache-Control.
+		r[1].merge!({'Vary' => 'Accept', 'Cache-Control' => 'public, max-age=120', 'Expires' => (Time.now + 120).rfc2822})
+		r
 	end
 end
--- controllers/index.rb
+++ controllers/index.rb
@@ -4,9 +4,21 @@ require 'lib/xapian_schema'
 class IndexController < ApplicationController
 	def template
 		if @req['q']
-			open('views/search.haml').read
+			if @content_type == 'application/atom+xml'
+				open('views/feed.haml').read
+			else
+				open('views/search.haml').read
+			end
 		else
 			open('views/index.haml').read
+		end
+	end
+
+	def recognized_types
+		if @req['q']
+			['text/html', 'application/xhtml+xml', 'application/atom+xml']
+		else
+			['text/html', 'application/xhtml+xml']
 		end
 	end
 
